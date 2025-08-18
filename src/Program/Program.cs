8ba3e13653ce4c6f452ae378bfbbb3c36d87243f
franciscamartin <franciscamartin080@gmail.com>
2025-08-18 08:43:16 -0300

    Update Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,8 +1,3 @@
-﻿Console.WriteLine(Addition.Add(7, 2));
-Console.WriteLine(Substraction.Substract(10, 1));
-Console.WriteLine(Multiplication.Multiply(3, 3));
-Console.WriteLine(Division.Divide(18, 2));
-
 // Esta clase implementa la operación RESTA
 public class Substraction
 {
